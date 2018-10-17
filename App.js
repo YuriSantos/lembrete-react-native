f932192e6099001d18ff4624f769ca2eb36fd3e2
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2018-10-17 10:25:46 -0300

    Colocando uma lista estatica

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    lembretes: []
+  };
+
+    componentDidMount () {
+        fetch('https://devreminder.herokuapp.com/lembrete', { method: 'GET' })
+            .then(T => T.json())
+            .then(lembretes => this.setState({ lembretes }))
+    }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Lembrete</Text>
+        <Button title='Adicionar' onPress={() => console.log('Criou o lembrete')}/>
+        <View>
+          <View>
+            <Text>Lembrete 1</Text>
+              <Button title='Editar' onPress={() => console.log('Editou')}/>
+              <Button title='Excluir' onPress={() => console.log('Excluiu')}/>
+          </View>
+            <View>
+                <Text>Lembrete 2</Text>
+                <Button title='Editar' onPress={() => console.log('Editou')}/>
+                <Button title='Excluir' onPress={() => console.log('Excluiu')}/>
+            </View>
+
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
